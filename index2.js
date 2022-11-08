0e24d4f493bad7bfc4c4dbe801471e26c6f32690
MackyClap <martimac2320@gmail.com>
2022-11-07 21:43:52 -0500

    refactor with promises

---
+++ index2.js
@@ -0,0 +1,9 @@
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes)
+  })
+  .catch((err) => {
+    console.log("It didn't work: ", err.message);
+  });
